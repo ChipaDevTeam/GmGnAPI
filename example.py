42fe8cb2851b664b258a9e9a161c9962f599c797
Vigo Walker <vigopaul05@gmail.com>
2026-02-02 18:33:44 +0100

    feat: Update cookie values and user UUID in GmGnClient initialization

--- example.py
+++ example.py
@@ -21,17 +21,17 @@ async def handle_token_launch(data):
 async def main():
     # Cookies from your browser session (required for connection)
     cookies = {
-        "__cf_bm": "AJoMSE2nVGkQBw517YaUKbnh2fTKue.rIiVFPk1adEI-1770052484-1.0.1.1-5_Ma1MK0d4faXcUfqgqkh3icGFL.qfTs0T9.alJfG45Nm9u7yGOaihmp0jQutIrJoP2Q5R6fpjVYmJHOtiKwPAwznSyHmerusjuzfP_LmN8",
         "_ga": "GA1.1.1679316283.1770052488",
         "sid": "gmgn%7Cb832083bf7577661314742d93fcc3fa4",
         "_ga_UGLVBMV4Z0": "GS1.2.1770052551374447.b660ff2d16ef55e527fbd41da44a650a.LPefI3GLwvJAZvcCerWQEA%3D%3D.VXM%2FiSQDiZQqEIk2yytV%2FQ%3D%3D.05A79i5172t4yba6oMhJuQ%3D%3D.oHMWEJIlhkk7YVBO3T8U5w%3D%3D",
-        "_ga_0XM0LYXGC8": "GS2.1.s1770052487$o1$g1$t1770052644$j60$l0$h0",
+        "_ga_0XM0LYXGC8": "GS2.1.s1770052487$o1$g1$t1770053193$j58$l0$h0",
+        "__cf_bm": "qyYtEiW3qfbG_sa_GAsVBjFWXfGawA3vH_eJTd6FZh4-1770053419-1.0.1.1-52SkQkftOyxkRyFNqniq4fuP2G_QoMa6WECZkSV7WcXZY0ueqHSZhUll8Q3QuiAEqPZmgpASgmSBBLQutm9iZQUXK9gma_inTTx8XNjWoG8",
     }
 
     # Use the device_id from the captured session
     device_id = "f3188121-8e81-4752-9eee-0ea242354a03"
     fp_did = "b4d8308ddd0df730f26ffdffcb5ecd03"
-    user_uuid = "f41873c323d65843" # 16 chars from curl (different from device_id)
+    user_uuid = "a5968838e0e7d892" # Updated from curl
     
     client = GmGnClient(cookies=cookies, device_id=device_id, fp_did=fp_did, user_uuid=user_uuid)
     
